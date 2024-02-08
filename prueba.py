e6c8bb0112dfe3b6d74be74fef0e1682d4cf5fee
PabloJavier22 <pmungar408@g.educaand.es>
2024-02-08 15:48:08 +0100

    HOLA
    DAWDAW

--- prueba.py
+++ prueba.py
@@ -1,11 +1,21 @@
+from statistics import variance
 from flask import Flask
 
 
 app = Flask(__name__)
 
+def Espanol():
+    español = "<p>Hola, Mundo!</p>"
+    return español
 
 @app.route("/")
+def hello_world(): 
+   
+    return  Espanol()+"<p>Hello, World!</p>"
+   
 
-def hello_world():
-
-    return "<p>Hello, World!</p>"
+@app.route("/returnList")
+def ReturnList():
+    resultado = "hola"
+    return "<p>"+resultado+"</p>"
+    
