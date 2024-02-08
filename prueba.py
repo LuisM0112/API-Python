daf6b108c3a095bd7828b30abb8fb5ba1104526a
LuisM0112 <149406770+LuisM0112@users.noreply.github.com>
2024-02-08 16:12:04 +0100

    Script Base de datos

--- prueba.py
+++ prueba.py
@@ -1,11 +1,20 @@
 from flask import Flask
+import sqlite3
 
+nombreBD = "homeX.db"
+# sentenciaTabla = "CREATE TABLE empleado(name, job)"
+
+# Se conecta a la base de datos, si no existe la crea
+connection = sqlite3.connect(nombreBD)
+
+# Crea un cursor para poder ejecutar sentencias SQLite
+cursor = connection.cursor()
+
+#Crea la entidad para la base de datos
+# cursor.execute(sentenciaTabla)
 
 app = Flask(__name__)
 
-
 @app.route("/")
-
 def hello_world():
-
     return "<p>Hello, World!</p>"
