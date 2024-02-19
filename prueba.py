b5ce47497b48af4b90cbb0a52947c5b8ac6826e4
PabloJavier22 <pmungar408@g.educaand.es>
2024-02-19 20:55:14 +0100

    Todos los select de productos

--- prueba.py
+++ prueba.py
@@ -2,26 +2,64 @@ import sqlite3
 
 from statistics import variance
 from flask import Flask
+from flask import request
 import sqlite3
 
 nombreBD = "HomeX.db"
-# sentenciaTabla = "CREATE TABLE empleado(name, job)"
 
-# Se conecta a la base de datos, si no existe la crea
-connection = sqlite3.connect(nombreBD)
-
-# Crea un cursor para poder ejecutar sentencias SQLite
-cursor = connection.cursor()
-
-#Crea la entidad para la base de datos
-# cursor.execute(sentenciaTabla)
-[]
-res = cursor.execute("SELECT name FROM Producto")
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
+def Inicio():
+    connection = sqlite3.connect(nombreBD)
+    cursor = connection.cursor()
+    res = cursor.execute("SELECT id, name, description, stock FROM Producto").fetchall()
+    connection.close()
+    return str(res)
 
-def hello_world():
+@app.route("/CrearProducto", methods=['POST'])
+def CrearProducto():
 
-    return "res"
+    product_name = request.form.get('name')
+    product_description = request.form.get('description')
+    product_stock = request.form.get('stock')
+    connection = sqlite3.connect(nombreBD)
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO Producto (name, description, stock) VALUES (?, ?, ?)",(product_name, product_description, product_stock))
+    connection.commit()
+    connection.close()
+    return "Producto Creado"
+
+@app.route("/EliminarProducto", methods=['POST'])
+def EliminarProducto():
+    product_id = request.forms.get('id')
+    connection = sqlite3.connect(nombreBD)
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM Producto WHERE id = "+product_id)
+    connection.commit()
+    connection.close()
+    return "Producto Eliminar"
+
+@app.route("/EditarProducto", methods=['PUT'])
+def EditarProducto():
+    product_id = request.args.get('id')
+    product_name = request.form.get('name')
+    product_description = request.form.get('description')
+    product_stock = request.form.get('stock')
+    connection = sqlite3.connect(nombreBD)
+    cursor = connection.cursor()
+    cursor.execute("UPDATE Producto SET name = ?, description = ?, stock = ? WHERE id = ?", (product_name, product_description, product_stock, product_id))
+    connection.commit()
+    connection.close()
+    return "Producto Actualizado"
+
+
+@app.route("/BuscarProducto", methods=['GET'])
+def BuscarProducto():
+    product_id = request.forms.get('id')
+    connection = sqlite3.connect(nombreBD)
+    cursor = connection.cursor()
+    res = cursor.execute("SELECT name, description, stock FROM Producto WHERE id = ?", (product_id,)).fetchall()
+    connection.close()
+    return(res)
